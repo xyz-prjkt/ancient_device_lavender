83d29be761dafefb6ccfd69fe92463cc90b7d0d9
xyzuan <i.xyzuan@gmail.com>
2020-12-15 11:39:12 +0000

    lavender: adapt sepolicy path.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -240,7 +240,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 PROTOBUF_SUPPORTED := true
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(DT_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DT_PATH)/sepolicy/public
