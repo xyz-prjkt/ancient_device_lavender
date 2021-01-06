eae18ba73864985ff58e9c18ed7badf9bb5eba5b
xyzuan <i.xyzuan@gmail.com>
2021-01-06 15:06:49 +0700

    lavender: fp: Redfin - January 2021

--- ancient_lavender.mk
+++ ancient_lavender.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 xyzprkjt
+# Copyright (C) 2020-2021 xyzprkjt
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -58,8 +58,8 @@ TARGET_VENDOR_PRODUCT_NAME := lavender
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ1A.201205.010 6953398 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ1A.210105.003 7005429 release-keys
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 PRODUCT_BUILD_PROP_OVERRIDES += \
