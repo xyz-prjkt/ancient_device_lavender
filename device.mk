7557dbb70cd10db51a36ce01c18497f64efe2cfd
OdSazib <odsazib@gmail.com>
2021-02-07 06:57:38 +0600

    lavender: Checkout rootdir and init stuffs from caf
    * adapt with eas and cleanup unnecessary stuffs
    * Fixed offline charging and usb functions
    
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- device.mk
+++ device.mk
@@ -262,7 +262,6 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi_parts.rc \
-    init.performance.sdm660.rc \
     ueventd.qcom.rc \
     init.verity.rc
 
