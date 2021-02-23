1063b0568c73afc9db72adb479e385f1aa712bb6
OdSazib <odsazib@gmail.com>
2021-02-23 19:44:52 +0600

    lavender: Uprev vendor.display.config to 2.0 New wfd blobs requires display config to be updated ---------------------------------------------------- CANNOT LINK EXECUTABLE "/vendor/bin/wifidisplayhalservice": library "vendor.display.config@2.0.so" not found: needed by /vendor/lib/libwfddisplayconfig_proprietary.so in namespace (default)

--- device.mk
+++ device.mk
@@ -167,8 +167,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
