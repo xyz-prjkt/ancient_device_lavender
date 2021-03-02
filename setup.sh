e53dcc7e2ccf13ea0cc6bc510578a9bc74379a52
Jody Yuantoro <i.xyzuan@gmail.com>
2021-03-03 06:33:22 +0700

    lavender: setup.sh: track our dirac repo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/xyz-prjkt/ancient_vendor_lavender ../../../vendor/xiaomi/lavender
+git clone https://github.com/xyz-prjkt/ancient_vendor_lavender ../../../vendor/xiaomi/lavender -b 11
 git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender ../../../kernel/xiaomi/lavender -b eas-oldcam
 git clone https://github.com/xyz-prjkt/hw_audio ../../../hardware/qcom-caf/xyz-sdm660/audio -b 11-sdm660
 git clone https://github.com/xyz-prjkt/hw_media ../../../hardware/qcom-caf/xyz-sdm660/media -b 11-sdm660
 git clone https://github.com/xyz-prjkt/hw_display ../../../hardware/qcom-caf/xyz-sdm660/display -b 11-sdm660
+git clone https://github.com/xyz-prjkt/vendor_xiaomi_dirac ../../../vendor/Xiaomi/dirac -b 11
