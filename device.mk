095a3a06d7876789ad646ea4d362db8c5fcb8dba
xyzuan <i.xyzuan@gmail.com>
2021-03-03 10:03:34 +0000

    lavender: build android.hardware.camera.device@3.5
    fixup : Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.device@3.5.so" not found

--- device.mk
+++ device.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
