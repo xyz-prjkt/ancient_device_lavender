c3287e2908fa50f7b944a3be3b5f4508fd022b75
xyzuan <i.xyzuan@gmail.com>
2021-03-03 23:03:14 +0000

    lavender: build vendor.display.config@2.0

--- device.mk
+++ device.mk
@@ -166,8 +166,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9.vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
