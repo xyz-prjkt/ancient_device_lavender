917cdf2cde3f658b9da83285659f81f51ca26470
Jody Yuantoro <i.xyzuan@gmail.com>
2021-03-03 20:39:21 +0700

    lavender: setup.sh: track  lavender-11-LA.UM.9.6.2.r1-03600-89xx.0 proprietary blobs

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-git clone https://github.com/xyz-prjkt/ancient_vendor_lavender ../../../vendor/xiaomi/lavender && git clone  https://github.com/stormbreaker-project/kernel_xiaomi_lavender ../../../kernel/xiaomi/lavender -b eas-oldcam
+git clone https://github.com/xyz-prjkt/ancient_vendor_lavender ../../../vendor/xiaomi/lavender -b 11-LA.UM.9.6.2.r1-03600-89xx.0
+git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender ../../../kernel/xiaomi/lavender -b eas-oldcam
+git clone https://github.com/xyz-prjkt/vendor_xiaomi_dirac ../../../vendor/Xiaomi/dirac -b 11
