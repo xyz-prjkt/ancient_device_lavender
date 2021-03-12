6da0e2c8ec163b925f2539c66aa4ee6ec8fc6965
xyzuan <i.xyzuan@gmail.com>
2021-03-12 13:52:39 +0700

    lavender: build android.hardware.thermal@1.0

--- device.mk
+++ device.mk
@@ -499,6 +499,12 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sdm660
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
