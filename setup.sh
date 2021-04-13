177b2bea8a4231e9521f07214f7a714bf3e03346
Jody Yuantoro <i.xyzuan@gmail.com>
2021-04-13 19:30:44 +0700

    lavender: setup.sh: track xbore.strmbreaker kernel source.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/xyz-prjkt/ancient_vendor_lavender ../../../vendor/xiaomi/lavender -b 11-LA.UM.9.6.2.r1-03600-89xx.0
-git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender ../../../kernel/xiaomi/lavender -b eas-oldcam
+git clone https://github.com/xyz-prjkt/xbore_strmbreakr_lavender ../../../kernel/xiaomi/lavender -b eleven-caf_eas
 git clone https://github.com/xyz-prjkt/vendor_xiaomi_dirac ../../../vendor/Xiaomi/dirac -b 11
 git clone https://github.com/kdrag0n/proton-clang ../../../prebuilts/clang/host/linux-x86/clang-proton_clang --depth=1
