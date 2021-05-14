526ebd37c07d606bd7356c27cca7f476ba5b3c27
dianlujitao <dianlujitao@lineageos.org>
2021-05-15 05:37:03 +0600

    lavender: Switch to Audio HAL v5 All devices use pre-R audio.bluetooth_qti.default.so and platina uses pre-R audio hal, in which the new set_event_callback API is not implemented and the audio service will crash due to mismatched struct.
    Change-Id: Ia330ac05d84d3dfb3a639458d66a3dc29689e67c

--- device.mk
+++ device.mk
@@ -38,9 +38,8 @@ TARGET_PRODUCT_PROP += $(DT_PATH)/xyz-prop/product.prop
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.service \
     audio.a2dp.default \
     audio.primary.sdm660 \
